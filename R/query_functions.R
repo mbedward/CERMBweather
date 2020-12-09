ad928e49b03d1c5d1f44ebf8b410de3b4faa379f
Michael Bedward <michael.bedward@gmail.com>
2020-12-09 18:17:35 +1100

    Function to get date range for each station

--- R/query_functions.R
+++ R/query_functions.R
@@ -131,3 +131,73 @@ bom_db_get_daily_rainfall <- function(db,
   }
 }
 
+
+#' Get first and last record dates for stations
+#'
+#' This function performs a fast query to find the dates of the earliest and
+#' the most recent record for each station.
+#'
+#' The speed of this query with large tables comes from the use of a LATERAL
+#' join. This is a type of join provided by PostgreSQL that allows a SQL query
+#' to function like a for-each loop. It also relies on the presence of an index
+#' on the date_local field. The approach is adapted from an example (number 5)
+#' in \href{https://stackoverflow.com/a/34715134/40246}{this StackOverflow answer.}
+#'
+#' @param db A database connection pool object.
+#'
+#' @param the.table The table to query: either 'aws' or 'synoptic'
+#'   (may be abbreviated).
+#'
+#' @param dry.run If \code{TRUE}, the function returns the SQL code for the
+#'   query but does not run it. This can be useful if you want to further
+#'   modify the query. If \code{FALSE} (default), the function runs the query
+#'   and returns the resulting data.
+#'
+#' @return A data frame with columns: station, earliest_date, latest_date.
+#'
+#' @export
+#'
+bom_db_get_station_dates <- function(db, the.table, dry.run = FALSE) {
+  if (!dry.run) .ensure_connection(db)
+
+  the.table <- match.arg(tolower(the.table), choices = c("aws", "synoptic"))
+
+  # lateral query template
+  cmd0 <- glue::glue("SELECT l.station, l.date_local as {{field.name}}
+                     FROM
+                       {the.table}_stations s,
+                       LATERAL (
+                        SELECT station, date_local
+                        FROM {the.table}
+                        WHERE station = s.station  -- lateral reference
+                        ORDER BY date_local {{asc.desc}}
+                        LIMIT 1
+                       ) l")
+
+  # Separate earliest and latest date queries
+  cmd.earliest <- glue::glue(cmd0,
+                             field.name = "earliest_date",
+                             asc.desc = "ASC")
+
+  cmd.latest <- glue::glue(cmd0,
+                           field.name = "latest_date",
+                           asc.desc = "DESC")
+
+  # Combined query
+  cmd.full <- glue::glue("SELECT early.station, earliest_date, latest_date
+                         FROM ({cmd.earliest}) early
+                         LEFT JOIN ({cmd.latest}) late
+                         ON early.station = late.station
+                         ORDER BY early.station;")
+
+  if (dry.run) {
+    # Just return composed query text
+    cmd.full
+  } else {
+    # Run query and return data
+    pool::dbGetQuery(db, cmd.full)
+  }
+}
+
+
+
